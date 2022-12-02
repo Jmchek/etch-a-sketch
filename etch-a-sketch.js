f053ece839cb94419267836d8103ef61eb1fb326
Joe Chek <jmchek@gmail.com>
2022-12-02 10:45:04 -0800

    create container div and .css file

---
+++ etch-a-sketch.js
@@ -0,0 +1,19 @@
+// Create a webpage with a 16x16 grid of square divs.
+// Create the divs using JavaScript. Don’t try making them by hand with copy and pasting in your HTML file!
+// It’s best to put your grid squares inside another “container” div (which can go directly in your HTML).
+// There are several different ways to make the divs appear as a grid (versus just one on each line). Feel free to use any or play with each of them:
+// float/clear
+// inline-block
+// flexbox
+// CSS Grid
+// Be careful with borders and margins, as they can adjust the size of the squares!
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// create 16x16 square divs using js only
+// center the container div to be more presentable
+// use loops to create the first row
+// create each grid by creating a new div and appending?
+// repeat until we hit the end of the 16th row and finish
+// place them in the "container" div
+// make them appear as a grid
