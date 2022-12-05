1d5ee5147d739827e509397f45a7facf9d2dfe25
Joe Chek <jmchek@gmail.com>
2022-12-05 14:32:34 -0800

    finish the loop to add the column divs and complete the grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -25,25 +25,44 @@
 // <div> 16xdivs </div> for the first loop let's append all to the container div
 // function loopMaker() will do just that
 //we will have to name each rowDiv() by number, or we can create an array of divs....
+// ^^^^ DONE ^^^^
 
-
+// for each row div, add 16 column divs or colDivs
+// create a loop that runs through each row, then adds 16 divs to them
+// add them to a colDivArr[] to track? let's try a 2d array
 // create each grid by creating a new div and appending?
 // create a class for the divs so that they behave and are placed correctly
 // repeat until we hit the end of the 16th row and finish
 // place them in the "container" div
 // make them appear as a grid
+// ^^^ DONE ^^^
+
+
 
 
 
 function loopMaker() {
     let rowDivArr = [];
+    let colDivArr = [];
     const grabContainer = document.querySelector("#container")
 
+    //create row divs
     for (let i = 0; i < 16; i++){
         rowDivArr[i] = document.createElement('div');
         rowDivArr[i].setAttribute('class', 'rowDiv');
         grabContainer.appendChild(rowDivArr[i]);
+        colDivArr[i] = [];
+
+        //create column divs 
+        for (let j = 0; j < 16; j++){
+            colDivArr[i][j] = document.createElement('div');
+            colDivArr[i][j].setAttribute('class', 'colDiv');
+            console.log(colDivArr);
+            rowDivArr[i].appendChild(colDivArr[i][j]);
+        }
     }
+
+    
 
 }
 
