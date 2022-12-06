2aaaa8fb04192601f9ddc50a1c48e1ed00c553ad
Joe Chek <jmchek@gmail.com>
2022-12-05 18:23:37 -0800

    add the hover effect for each div (simulate etch-a-sketch)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,7 @@
-// Create a webpage with a 16x16 grid of square divs.
-// Create the divs using JavaScript. Don’t try making them by hand with copy and pasting in your HTML file!
-// It’s best to put your grid squares inside another “container” div (which can go directly in your HTML).
-// There are several different ways to make the divs appear as a grid (versus just one on each line). Feel free to use any or play with each of them:
-// float/clear
-// inline-block
-// flexbox
-// CSS Grid
-// Be careful with borders and margins, as they can adjust the size of the squares!
-
+// Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
+// Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
+// Also check out prompts.
+// You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used.
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // create 16x16 square divs using js only
@@ -38,6 +32,15 @@
 // ^^^ DONE ^^^
 
 
+// add hover effect to each column div
+// the hover effect is to stay
+// when the hover effect is applied, set the class to "etchedDiv"
+// add event listener for a hover, then apply "etchedDiv"
+// ^^^ DONE ^^^ 
+
+
+
+
 
 
 
@@ -57,7 +60,9 @@ function loopMaker() {
         for (let j = 0; j < 16; j++){
             colDivArr[i][j] = document.createElement('div');
             colDivArr[i][j].setAttribute('class', 'colDiv');
-            console.log(colDivArr);
+            colDivArr[i][j].addEventListener('mouseover', () => {
+                colDivArr[i][j].classList.add('etchedDiv');
+              });
             rowDivArr[i].appendChild(colDivArr[i][j]);
         }
     }
